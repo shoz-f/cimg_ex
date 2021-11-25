d1fd350aa1de77bd8cfe94a1ab57e856d86451c7
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2021-11-26 05:59:57 +0900

    prepare refactoring

---
+++ src/cimg_nif.h
@@ -0,0 +1,32 @@
+/***  File Header  ************************************************************/
+/**
+* cimg_nif.h
+*
+* Elixir/Erlang extension module: CImg
+* @author Shozo Fukuda
+* @date	  Thu Nov 25 18:45:58 JST 2021
+* System  MINGW64/Windows, Ubuntu/WSL2<br>
+*
+**/
+/**************************************************************************{{{*/
+
+#include "CImgEx.h"
+#include "my_erl_nif.h"
+
+// 認識アプリ・パッケージ
+//namespace RC_APP_package {
+	
+/*---     定数定義     ---*/
+
+/*---      型定義      ---*/
+
+/*---    マクロ定義    ---*/
+
+/*---  外部モジュール  ---*/
+
+/*---     外部変数     ---*/
+
+/*---     内部変数     ---*/
+
+/*---  内部モジュール  ---*/
+
