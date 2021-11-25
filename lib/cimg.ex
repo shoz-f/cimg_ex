86b78b40197e36fa9232597b6cd5cd3497d33865
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2021-11-26 05:58:35 +0900

    update documents

--- lib/cimg.ex
+++ lib/cimg.ex
@@ -1,6 +1,36 @@
 defmodule CImg do
   @moduledoc """
-  Light-weight image processing extention.
+  Light-weight image processing module in Elixir with CImg. This module aims to 
+  create auxiliary routines for Deep Learning.
+
+  Note: It still has a few image processing functions currentrly.
+  
+  ## Design detail
+
+  
+  ## Platform
+  It has been confirmed to work in the following OS environment.
+
+  - Windows MSYS2/MinGW64
+  - WSL2/Ubuntu 20.04
+
+  ## Demo
+  There is a simple program in demo directory. You can do it by following the steps below.
+
+  ```shell
+  $ cd demo
+  $ mix deps.get
+  $ mix run -e "CImgDemo.demo(1)"
+  ```
+
+  Close the appaired window, and stop the demo program.
+
+  Another demo:
+
+  ```shell
+  $ mix run -e "CImgDemo.demo(2)"
+  ```
+
   """
   alias __MODULE__
   alias CImg.NIF
